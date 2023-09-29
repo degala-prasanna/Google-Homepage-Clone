a32c9a8b6295987fe83510674b58100bced39921
Degala Sai Prasanna <94045180+degala-prasanna@users.noreply.github.com>
2023-09-29 14:16:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
-let userInput=document.querySelector(".search-input");
-let searchbtn=document.querySelector(".search-btn");
+// Select the elements from the HTML using querySelector
+let userInput = document.querySelector(".search-input");
+let searchbtn = document.querySelector(".search-btn");
 
-
-function searchUserinput(){
-    let url="https://www.google.com/search?q="+userInput.value;
+// Define a function to perform the regular Google search
+function searchUserinput() {
+    // Create a URL by combining the Google search URL with the user's input
+    let url = "https://www.google.com/search?q=" + userInput.value;
+    
+    // Open the URL in the same tab/window
     window.open(url, "_self");
 };
 
-searchbtn.addEventListener("click",searchUserinput);
+// Add a click event listener to the search button to trigger the regular search
+searchbtn.addEventListener("click", searchUserinput);
 
-userInput.addEventListener("keydown",(event)=>{
-    if (event.key==="Enter"){
+// Add a keydown event listener to the input field to trigger the regular search on Enter key press
+userInput.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
         searchUserinput();
-       
     }
-})
+});
+
+// Define a function to perform the "I'm Feeling Lucky" search
+function feelingLuckybtn() {
+    // Define the URL for the "I'm Feeling Lucky" search
+    let url = "https://www.google.com/doodles";
+    
+    // Open the URL in the same tab/window
+    window.open(url, "_self");
+}
